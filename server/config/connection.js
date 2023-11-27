9c5a820953ab2a206a2b5564b0b58abd2a94a582
faysia1 <faysialopez@yahoo.com>
2023-11-26 21:28:20 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/BookSearch', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/BookSearch');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,38 +1,52 @@
-require("dotenv").config();
-const express = require('express');
-// Import the ApolloServer class
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
+const express = require("express");
+const path = require("path");
+//import apollo server
+const { ApolloServer } = require("apollo-server-express");
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
-// Import the two parts of a GraphQL schema
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
+//db connection
+const db = require("./config/connection");
 
+// const routes = require('./routes');
+
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-server.applyMiddleWare({ app });
+//apply apollo server with express app
+server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+//middleware parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+app.use(express.static(buildPath));
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// app.use(routes);
+
+//get all
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`Now listening on port: ${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
